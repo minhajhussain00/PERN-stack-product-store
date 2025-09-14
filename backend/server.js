eb33c58d753f6064f3d845aefc3a9dc508b76ac9
Minhaj Mehboob <minhaj202100294@uswagb.edu.pk>
2025-09-14 21:02:55 +0500

    feat: rate limiting and bot detection

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { sql } from "./config/db.js";
 import productRoutes from "./routes/productsRoutes.js"
+import { aj } from "./lib/arcjet.js";
 dotenv.config();
 const app = express();
 
@@ -12,7 +13,30 @@ app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors())
-
+app.use(async (req, res, next) => {
+    try {
+        const decision = await aj.protect(req,{requested:1});
+        if(decision.isDenied()){
+            if(decision.reason.isRateLimit()){
+                return res.status(429).json({message:"Too many requests - try again later"})
+            }
+            else if(decision.reason.isBot()){
+                return res.status(403).json({message:"Access denied - bots are not allowed"})
+            }else{
+                return res.status(403).json({message:"Access denied"})
+            }
+            return;
+        }
+        //chckt spoof bot
+        if(decision.results.some((results)=>results.reason.isBot() && results.reason.isSpoofed())){
+            return res.status(403).json({message:"Access denied - spoofed bots are not allowed"})
+        }
+        next()
+    } catch (error) {
+        console.error("Error in Arcjet middleware:", error);
+        res.status(500).json({message:"Internal server error"})
+    }
+})
 
 const PORT = process.env.PORT || 3000;
 
