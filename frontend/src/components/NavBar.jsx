dde80c10c9bb57e8cf7815ed39771ac7bc9b8894
Minhaj Mehboob <minhaj202100294@uswagb.edu.pk>
2025-09-21 16:21:52 +0500

    feat/navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { Link, useResolvedPath } from "react-router-dom";
+import { ShoppingBagIcon, ShoppingCartIcon } from "lucide-react";
 
 const NavBar = () => {
+  const { pathname } = useResolvedPath();
+  const isHomePage = pathname === "/";
+  const productDummy = [1, 2, 3, 4, 5]; 
+
   return (
-    <div>
-      
+    <div className='bg-base-100/80 backdrop-blur-lg border-b border-base-300 sticky top-0 z-50'>
+      <div className='max-w-7xl mx-auth'>
+
+        <div className='navbar px-4 min-h-[4rem] justify-between'>
+            <div className="flex-1 lg:flex-none">
+            <Link to="/" className="hover:opacity-80 transition-opacity">
+              <div className="flex items-center gap-2">
+                <ShoppingCartIcon className="size-9 text-primary" />
+                <span
+                  className="font-semibold font-mono tracking-widest text-2xl 
+                    bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"
+                >
+                  STORE
+                </span>
+              </div>
+            </Link>
+          </div>
+
+          <div className="flex items-center gap-4">
+
+            {isHomePage && (
+              <div className="indicator">
+                <div className="p-2 rounded-full hover:bg-base-200 transition-colors">
+                  <ShoppingBagIcon className="size-5" />
+                  <span className="badge badge-sm badge-primary indicator-item">
+                    {productDummy.length}
+                  </span>
+                </div>
+              </div>
+            )}
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
